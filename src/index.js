bd1737e70ead593ea6827f1af7c5a130ea880f4e
Abdulmujeeb Sarfaraz <Abdulmuji17@gmail.com>
2026-07-24 19:29:57 +0500

    Day 1 - Project Setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("ExpenseBot AI is running 🚀");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
